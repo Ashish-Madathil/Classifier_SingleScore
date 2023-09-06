61c78958630a22d5744d9291c47edd9bdd26014e
Progenesis-AI <142873340+Progenesis-AI@users.noreply.github.com>
2023-09-06 16:36:20 +0530

    modified

--- Classifier.py
+++ Classifier.py
@@ -1,2 +1,3 @@
 #multiclass image classification model and a training script for a custom dataset that pulls from a csv file consisting of image paths in the first column and an integer of any value between 0 and 4 in the second column. Using PyTorch.
 import torch.nn as nn
+
