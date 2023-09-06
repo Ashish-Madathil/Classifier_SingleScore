366267d579061a8034bff03f3f4b5412824285e3
Ashish <amadathil@progenesis.com>
2023-09-06 15:41:55 +0530

    Modified Dataset Class

--- embryo_dataset.py
+++ embryo_dataset.py
@@ -5,17 +5,20 @@ import pandas as pd
 from PIL import Image
 
 class EmbryoDataset(Dataset):
-    def __init__ (self, csv_path, transform=None):
-        self.data = pd.read_csv(csv_path)
+    def __init__ (self, txt_path, transform=None):
+        with open(txt_path, 'r') as f:
+            lines = f.readlines()
+        self.img_list = [line.split()[0] for line in lines]
+        self.label_list = [line.split()[1] for line in lines]
         self.transform = transform
 
     def __len__(self):
-        return len(self.data)
+        return len(self.img_list)
     
     def __getitem__ (self, index):
-        img_path = self.data.iloc[index, 0]
-        image = Image.open(img_path)
-        label = self.data.iloc[index, 1]
+        img_path = self.img_list[index]
+        image = Image.open(img_path).convert('RGB')
+        label = self.label_list[index]
         if self.transform:
             image = self.transform(image)
         
