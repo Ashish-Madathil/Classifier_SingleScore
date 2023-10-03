8709d30956be3db1d29de604062729db93a9753d
Progenesis-AI <142873340+Progenesis-AI@users.noreply.github.com>
2023-10-03 19:09:34 +0530

    modified main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 # External imports
 import torch
 import torch.nn as nn
@@ -13,29 +12,28 @@ from sklearn.utils.class_weight import compute_class_weight
 # import sys
 import logging
 import numpy as np
+from sklearn.metrics import confusion_matrix, cohen_kappa_score
+import seaborn as sns
 
 # Local imports
 from models import *
-
 from stratified_sampling import split_indices
 from embryo_dataset import EmbryoDataset
-
 
 
 # Hyperparameters
 learning_rate = 1e-6
 lower_lr = 0.000001
 batch_size = 64
-num_epochs = 500
-# NUM_EPOCHS_BEFORE_FINE_TUNING = 150
-losses = []
+num_epochs = 300
 
+losses = []
 
 
 # Set up logging
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(message)s',
-                    handlers=[logging.FileHandler("training_log_MobileNetV2_1809_wd0_5_drop0_2.txt"),
+                    handlers=[logging.FileHandler("training_log_resnet50_0310.txt"),
                               logging.StreamHandler()])  # StreamHandler is for console
 logger = logging.getLogger()
 
@@ -46,20 +44,26 @@ logger = logging.getLogger()
 # ])
 train_transform = transforms.Compose([
     # transforms.RandomResizedCrop(224, scale=(0.8, 1.0)),
-    transforms.RandomHorizontalFlip(),
-    transforms.RandomRotation(degrees=15),
-    transforms.ColorJitter(brightness=0.15, contrast=0.15, saturation=0.15, hue=0.1),
+    transforms.Resize((224,224)),
+    # transforms.Pad((7, 7)),
+    # transforms.RandomHorizontalFlip(),
+    # transforms.RandomRotation(degrees=15),
+    # transforms.ColorJitter(brightness=0.15, contrast=0.15, saturation=0.15, hue=0.1),
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+    # transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
 val_transform = transforms.Compose([
+        transforms.Resize((224,224)),
+        # transforms.Pad((7, 7)),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+        # transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
 
 full_dataset = EmbryoDataset(txt_path="ed4_as_target.txt")
-train_indices, val_indices, test_indices = split_indices(len(full_dataset), train_pct=0.6, val_pct=0.2, seed=44, stratify=full_dataset.label_list)
+train_indices, val_indices, test_indices = split_indices(len(full_dataset), train_pct=0.6, val_pct=0.2, seed=18, stratify=full_dataset.label_list)
 logger.info(f"{len(train_indices)}, {len(val_indices)}, {len(test_indices)}")
 
 train_dataset = EmbryoDataset(txt_path="ed4_as_target.txt", transform=train_transform)
@@ -74,30 +78,20 @@ train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, sampler=
 val_loader = DataLoader(dataset=val_dataset, batch_size=batch_size, sampler=val_sampler)
 test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, sampler=test_sampler)
 
-
+# for images, labels in train_loader:
+#     print(images.size(), labels.size())
+#     break
 
 # Initialize Model and Optimizer
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-# model = Classifier().to(device)
-# model = ResNet50Classifier(num_classes=5).to(device)
-# model = MobileNetV2Classifier(num_classes=5).to(device)
-model = SingleLabelMultiClassModel(num_classes=5).to(device)
-# model = ResNet34Classifier(num_classes=5).to(device)
-# model = EfficientNetClassifier(num_classes=5).to(device)
-# model = GoogLeNetClassifier(num_classes=5).to(device)
 
+model = ResNet50Classifier(num_classes=5).to(device)
 
+# model = SingleLabelMultiClassModel(num_classes=5).to(device)
 
-# # Freeze all layers
-# for param in model.resnet50.parameters():
-#     param.requires_grad = False
-
-# # Unfreeze the last layer/classifier layer (fully connected layer)
-# for param in model.resnet50.fc.parameters():
-#     param.requires_grad = True
 
 labels = [int(label) for label in full_dataset.label_list]
-class_counts = np.bincount(labels)
+# class_counts = np.bincount(labels)
 # total_samples = len(labels)
 # class_weights = total_samples / (len(class_counts) * class_counts)
 # class_weights = torch.FloatTensor(class_weights).to(device)
@@ -106,13 +100,16 @@ class_weights = torch.FloatTensor(class_weights).to(device)
 
 
 criterion = nn.CrossEntropyLoss(weight=class_weights)
-optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=0.5)
+optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-4)
+
 
 scheduler = ReduceLROnPlateau(optimizer, 'min')  # 'min' indicates reducing LR when a quantity (val loss in our case) stops decreasing
-# scheduler = OneCycleLR(optimizer, max_lr=learning_rate, epochs=num_epochs, steps_per_epoch=len(train_loader))
+
 
 num_classes = 5
 true_positives = torch.zeros(num_classes)
+false_positives = torch.zeros(num_classes)
+false_negatives = torch.zeros(num_classes)
 valloss =[]
 val_acc = []
 
@@ -127,13 +124,8 @@ epochs_no_improve = 0
 patience_epochs = 10
 fine_tuning_started = False # Flag to indicate if fine-tuning has started
 
-# Training Loop
+# for epoch in range(300, total_epochs):
 for epoch in range(num_epochs):
-    # Check if we should start fine-tuning
-    # if epoch == NUM_EPOCHS_BEFORE_FINE_TUNING:
-    #     for param in model.resnet18.parameters():
-    #         param.requires_grad = True
-    #     optimizer = optim.Adam(model.parameters(), lr=lower_lr, weight_decay=1e-4)
     model.train()
     tr_loss = 0
     for images, labels in train_loader:
@@ -143,7 +135,6 @@ for epoch in range(num_epochs):
         # Forward pass
         outputs = model(images)
         loss = criterion(outputs, labels)
-        # losses.append(loss.detach().cpu().numpy())
         tr_loss += loss.item()
 
         # Backward pass and optimization
@@ -153,12 +144,17 @@ for epoch in range(num_epochs):
 
     tr_loss /= len(train_loader)  #average loss per batch for the epoch
     losses.append(tr_loss)
+
     # Validation
     model.eval()
     val_loss = 0
     correct = 0
     true_positives.fill_(0)
+    false_positives.fill_(0)
+    false_negatives.fill_(0)
     total_per_class = torch.zeros(num_classes)
+    false_positives = torch.zeros(num_classes)
+    false_negatives = torch.zeros(num_classes)
 
     with torch.no_grad():
         for images, labels in val_loader:
@@ -169,9 +165,13 @@ for epoch in range(num_epochs):
             correct += predicted.eq(labels).sum().item()
 
             for i in range(num_classes):
+                # true_positives[i] += (predicted == labels).logical_and(predicted == i).sum().item()
                 true_positives[i] += ((predicted == labels) & (predicted == i)).sum().item()
+                false_negatives[i] += ((predicted != labels) & (labels == i)).sum().item()
+                false_positives[i] += ((predicted == i) & (predicted != labels)).sum().item()
                 total_per_class[i] += (labels == i).sum().item()
 
+    # accuracy = 100. * correct / len(val_loader.dataset)
     val_loss /= len(val_loader)
     accuracy = 100. * correct / len(val_indices)
 
@@ -181,79 +181,72 @@ for epoch in range(num_epochs):
     valloss.append(val_loss)
     val_acc.append(accuracy)
 
-    # if val_loss < best_val_loss:
-    #     best_val_loss = val_loss
-    #     epochs_no_improve = 0
-    # else:
-    #     epochs_no_improve += 1
 
-    # # If patience is reached and fine-tuning hasn't started, start fine-tuning
-    # if epochs_no_improve == patience_epochs and not fine_tuning_started:
-    #     print("Starting fine-tuning...")
-    #     # Unfreeze all parameters of the model
-    #     for param in model.resnet50.parameters():
-    #         param.requires_grad = True
-
-    #     optimizer = optim.Adam(model.parameters(), lr=lower_lr, weight_decay=1e-4)
-    #     # Set flag to prevent multiple starts
-    #     fine_tuning_started = True
-    #     epochs_no_improve = 0  # reset for the next phase
 
     # Early Stopping
     if val_loss < best_valid_loss:
         best_valid_loss = val_loss
         best_valid_epoch = epoch
         # Save the model checkpoint whenever validation loss improves
-        torch.save(model.state_dict(), 'model_MobileNetV2_1809_wd0_5_drop0_2.pth')
+        torch.save(model.state_dict(), 'model_resnet50_0310.pth')
     if epoch - best_valid_epoch >= patience:
         logger.info(f"Validation loss hasn't improved for {patience} epochs. Stopping training.")
         break
 
     logger.info(f"Epoch [{epoch+1}/{num_epochs}], Train Loss: {loss.item():.4f}, Val Loss: {val_loss:.4f}, Val Accuracy: {100. * correct / len(val_indices):.2f}%")
     for i in range(num_classes):
+        fnr = false_negatives[i] / (false_negatives[i] + true_positives[i])
+        fpr = false_positives[i] / (false_positives[i] + (total_per_class[i] - true_positives[i]))
         logger.info(f"Class {i} Recall: {true_positives[i] / total_per_class[i]:.2f} True Positives: {true_positives[i]}/{total_per_class[i]}")
+        logger.info(f"Class {i} False Positive Rate: {fpr:.2f}")
+        logger.info(f"Class {i} False Negative Rate: {fnr:.2f}")
 
 # Save model
-torch.save(model.state_dict(), f'model_MobileNetV2_1809_wd0_5_drop0_2.pth')
+torch.save(model.state_dict(), f'model_resnet50_2109.pth')
 logger.info('Finished Training. Model Saved')
+
+
 # Graph it out!
+plt.figure(figsize=(8, 6))
 
+# Plotting training loss with blue color (by default)
+plt.plot(losses, label="Training Loss")
+# Plotting validation loss with orange color (by default)
+plt.plot(valloss, label="Validation Loss")
 
-plt.figure(figsize=(12, 6))
-plt.subplot(1, 2, 1)
-plt.plot(losses)
-plt.ylabel("Training Loss")
+plt.ylabel("Loss")
 plt.xlabel('Epoch')
-plt.title("Training Loss over Epochs")
-
-
-
-plt.subplot(1, 2, 2)
-plt.plot(valloss)
-plt.ylabel("Validation Loss")
-plt.xlabel('Epoch')
-plt.title("Validation Loss over Epochs")
+plt.title("Training and Validation Loss over Epochs")
+plt.legend(loc="upper right")  # Add a legend
 plt.tight_layout() 
-plt.savefig("Losses_plot_MobileNetV2_1809_wd0_5_drop0_2.png", dpi=300)
-plt.clf()
+plt.savefig("Losses_plot_resnet50_0310.png", dpi=300)
+# plt.show()
 
+plt.clf()
 
 plt.plot(val_acc)
 plt.ylabel("Validation Accuracy")
 plt.xlabel('Epoch')
 plt.title("Validation Accuracy over Epochs")
-plt.savefig("val_accuracy_plot_MobileNetV2_1809_wd0_5_drop0_2.png", dpi=300)
+plt.savefig("val_accuracy_plot_resnet50_0310.png", dpi=300)
 
 
 
 #INFERENCE
 
-def infer_and_write_results(model, dataloader, indices, dataset, device, csv_filename='results__MobileNetV2_1809_wd0_5_drop0_2.csv'):
+def infer_and_write_results(model, dataloader, indices, dataset, device, csv_filename='results_resnet50_0310.csv'):
     """
     Use the trained model to make predictions on the dataloader and save results in a CSV.
     """
     model.eval()
     correct = 0
+    true_labels = []
+    predicted_labels = []
+    true_positives = torch.zeros(num_classes)
+    false_positives = torch.zeros(num_classes)
+    false_negatives = torch.zeros(num_classes)
+    true_negatives = torch.zeros(num_classes)  # New variable to track TN
+
     # Open a CSV file to write results
     with open(csv_filename, 'w', newline='') as csv_file:
         csvwriter = csv.writer(csv_file)
@@ -269,14 +262,41 @@ def infer_and_write_results(model, dataloader, indices, dataset, device, csv_fil
                 outputs = model(images)
                 _, predicted = outputs.max(1)
 
+                # Update confusion matrix
+                for i in range(num_classes):
+                    true_positives[i] += ((predicted == labels) & (predicted == i)).sum().item()
+                    false_negatives[i] += ((predicted != labels) & (labels == i)).sum().item()
+                    false_positives[i] += ((predicted == i) & (predicted != labels)).sum().item()
+                    true_negatives[i] += ((predicted != i) & (labels != i)).sum().item()
+
                 # Assuming dataloader returns batches of data in sequential order from dataset
                 for idx, (pred, gt) in enumerate(zip(predicted, labels)):
+                    true_labels.append(gt.item())
+                    predicted_labels.append(pred.item())
                     img_path = dataset.img_list[dataloader.sampler.indices[idx]]
                     csvwriter.writerow([img_path, pred.item(), gt.item()])
                     # Correct or not
                     if pred.item() == gt.item():
                         correct +=1
+    conf_matrix = confusion_matrix(true_labels, predicted_labels)
     accuracy = 100. * correct / len(indices)
+    overall_fpr = false_positives.sum().item() / (false_positives.sum().item() + true_negatives.sum().item())
+    overall_fnr = false_negatives.sum().item() / (false_negatives.sum().item() + true_positives.sum().item())
+    plt.figure(figsize=(10,7))
+    sns.heatmap(conf_matrix, annot=True, fmt='g', cmap='Blues')
+    plt.xlabel('Predicted labels')
+    plt.ylabel('True labels')
+    plt.title('Confusion Matrix')
+    plt.savefig("confusion_matrix_resnet50_0310.png", dpi=300)
+    # plt.show()
+    qwk = cohen_kappa_score(true_labels, predicted_labels, weights='quadratic')
+    logger.info(f'Quadratic Weighted Kappa (QWK) Score: {qwk:.4f}')
+
+
+
+    logger.info(f'Overall False Positive Rate: {overall_fpr:.2f}')
+    logger.info(f'Overall False Negative Rate: {overall_fnr:.2f}')
+
     logger.info(f'Accuracy on test data: {accuracy:.2f}% ({correct}/{len(indices)})')
 
 
@@ -284,8 +304,13 @@ def infer_and_write_results(model, dataloader, indices, dataset, device, csv_fil
     logger.info('######### END #########')
 
 # Load trained model
-model_path = "model_MobileNetV2_1809_wd0_5_drop0_2.pth"
-model = SingleLabelMultiClassModel(num_classes=5).to(device)
+model_path = "model_resnet50_2109.pth"
+model = ResNet50Classifier(num_classes=5).to(device)
+
+# model = SingleLabelMultiClassModel(num_classes=5).to(device)
+
 model.load_state_dict(torch.load(model_path))
 
-infer_and_write_results(model, test_loader, test_indices, full_dataset, device, 'results_MobileNetV2_1809_wd0_5_drop0_2.csv')
+# Call the function
+infer_and_write_results(model, test_loader, test_indices, full_dataset, device, 'results_resnet50_0310.csv')
+
